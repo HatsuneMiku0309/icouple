b2bf9c5f42222c2cfc5f929860d535c8a8cca9ce
cosmo <zhixuan0309@gmail.com>
2022-01-05 19:26:14 +0800

    fixed menu good

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -11,24 +11,19 @@
     <div class="flex-grow overflow-hidden p-2">
       <router-view class="h-full w-full"/>
     </div>
-    <div class="flex flex-row flex-grow-0 flex-shrink-0 w-full justify-around h-14 bg-green-300 items-center">
-      <router-link class="flex p-3" to="/"><i class="couple-icon home before:bg-cover before:bg-no-repeat before:w-7 before:h-7 md:before:w-8 md:before:h-8 before:relative before:inline-block"></i><!--<span>Home</span>--></router-link>
-      <router-link class="flex p-3" to="/fate"><i class="couple-icon fate before:bg-cover before:bg-no-repeat before:w-7 before:h-7 md:before:w-8 md:before:h-8 before:relative before:inline-block"></i><!--<span>Fate</span>--></router-link>
-      <router-link class="flex p-3" to="/"><i class="couple-icon chat before:bg-cover before:bg-no-repeat before:w-7 before:h-7 md:before:w-8 md:before:h-8 before:relative before:inline-block"></i><!--<span>Chat</span>--></router-link>
-      <router-link class="flex p-3" to="/"><i class="couple-icon date before:bg-cover before:bg-no-repeat before:w-7 before:h-7 md:before:w-8 md:before:h-8 before:relative before:inline-block"></i><!--<span>Date</span>--></router-link>
-      <router-link class="flex p-3" to="/"><i class="couple-icon me before:bg-cover before:bg-no-repeat before:w-7 before:h-7 md:before:w-8 md:before:h-8 before:relative before:inline-block"></i><!--<span>Me</span>--></router-link>
-    </div>
+    <Menu />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue';
+import { defineComponent, reactive, ref } from 'vue';
 import { useRouter } from 'vue-router';
 import Heart from '../components/Loaders/Heart.vue';
+import Menu from '../components/Menu.vue';
 
 export default defineComponent({
   name: 'Index',
-  components: { Heart },
+  components: { Heart, Menu },
   setup() {
     const router = useRouter();
     const isShowLoad = ref(false);
@@ -90,26 +85,6 @@ export default defineComponent({
 
 .couple-icon.setting::before {
   background-image: url("@/assets/images/svg/fi-rr-settings.svg");
-}
-
-.couple-icon.home::before {
-  background-image: url("@/assets/images/svg/fi-rr-home.svg");
-}
-
-.couple-icon.fate::before {
-  background-image: url("@/assets/images/svg/fi-rr-following.svg");
-}
-
-.couple-icon.chat::before {
-  background-image: url("@/assets/images/svg/fi-rr-comment-heart.svg");
-}
-
-.couple-icon.date::before {
-  background-image: url("@/assets/images/svg/fi-rr-hand-holding-heart.svg");
-}
-
-.couple-icon.me::before {
-  background-image: url("@/assets/images/svg/fi-rr-comment-user.svg");
 }
 
 ::-webkit-scrollbar {
