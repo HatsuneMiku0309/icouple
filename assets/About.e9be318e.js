d964a725460ce64ac833c3815526365094db2617
githubaction <githubaction@fake.com>
2022-03-17 10:02:48 +0000

    deploy to Github pages

---
+++ assets/About.e9be318e.js
@@ -0,0 +1,1 @@
+import{o as a,b as s,h as o}from"./vendor.2172747c.js";const n={},r={class:"about"},t=[o("h1",null,"This is an about page",-1)];n.render=function(o,n){return a(),s("div",r,t)};export{n as default};
